82e2134545870560d0ce3917194a99f847994a03
ca5p3r <ca5p3r1@icloud.com>
2021-07-16 14:38:57 +0200

    remove redundant action

--- src/reducers/map.js
+++ src/reducers/map.js
@@ -3,8 +3,7 @@ const mapInfoState = {
     mapCenter: [3379498.795126273, 3114399.982142698],
     mapZoom: 6.8282764481106195,
     defaultExtent: [2099724.358397806, 2504130.7943378426, 4659273.23185474, 3724669.1699475534],
-    mapExtent: [],
-    activeLayers: []
+    mapExtent: []
 };
 export const mapInfoReducer = (
     state = mapInfoState,
@@ -25,11 +24,6 @@ export const mapInfoReducer = (
             return {
                 ...state,
                 mapZoom: action.payload
-            };
-        case 'setLayers':
-            return {
-                ...state,
-                activeLayers: action.payload
             };
         case 'setMapExtent':
             return {
