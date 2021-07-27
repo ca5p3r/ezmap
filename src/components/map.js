4f9ce1f1eeea476941efdaf5190d0e54d07729d2
ca5p3r <ca5p3r1@icloud.com>
2021-07-27 17:48:05 +0200

    handle no queriable layers in identify

--- src/components/map.js
+++ src/components/map.js
@@ -171,51 +171,61 @@ const MyMap = () => {
                 const controller = new AbortController();
                 const buffer = makeBuffer(mapInfo.clickedPoint);
                 const queriableLayers = data.filter(item => item.geometry !== null);
-                queriableLayers.forEach(layer => {
-                    const coords = [transform(buffer.p1, 'EPSG:3857', layer.crs).join(' '), transform(buffer.p2, 'EPSG:3857', layer.crs).join(' '), transform(buffer.p3, 'EPSG:3857', layer.crs).join(' '), transform(buffer.p4, 'EPSG:3857', layer.crs).join(' '), transform(buffer.p5, 'EPSG:3857', layer.crs).join(' ')];
-                    const queryParam = coords.join(' ');
-                    let raw = `<wfs:GetFeature service="WFS" outputFormat="application/json" version="1.1.0" xmlns:topp="http://www.openplans.org/topp" xmlns:wfs="http://www.opengis.net/wfs" xmlns="http://www.opengis.net/ogc" xmlns:gml="http://www.opengis.net/gml" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.1.0/wfs.xsd"><wfs:Query typeName="${layer.name}"><Filter><Intersects><PropertyName>${layer.geometry}</PropertyName><gml:Polygon srsName="${layer.crs}"><gml:exterior><gml:LinearRing><gml:posList>${queryParam}</gml:posList></gml:LinearRing></gml:exterior></gml:Polygon></Intersects></Filter></wfs:Query></wfs:GetFeature>`;
-                    let requestOptions = {
-                        method: 'POST',
-                        body: raw,
-                        mode: 'cors',
-                    };
-                    fetch(layer.url + 'wfs', requestOptions)
-                        .then(res => {
-                            if (!res.ok) {
-                                dispatch(setToastColor('danger'));
-                                dispatch(setMessage({
-                                    title: 'Fetch error',
-                                    message: 'Could not fetch data!'
-                                }));
-                            };
-                            return res.json();
-                        })
-                        .then(obj => {
-                            if (obj.features[0]) {
-                                dispatch(setResult(obj.features[0]));
-                                dispatch(triggerIdentifyVisibility(true));
-                            }
-                            else {
-                                dispatch(setToastColor('warning'));
-                                dispatch(setMessage({
-                                    title: 'Warning',
-                                    message: 'No results found!'
-                                }));
-                                dispatch(triggerShowToast(true));
-                            }
-                        })
-                        .catch(error => {
-                            if (error.name !== 'AbortError') {
-                                dispatch(setToastColor('danger'));
-                                dispatch(setMessage({
-                                    title: 'Fetch error',
-                                    message: error.toString()
-                                }));
-                                dispatch(triggerShowToast(true));
-                            };
-                        });
-                })
+                if (queriableLayers.length > 0) {
+                    queriableLayers.forEach(layer => {
+                        const coords = [transform(buffer.p1, 'EPSG:3857', layer.crs).join(' '), transform(buffer.p2, 'EPSG:3857', layer.crs).join(' '), transform(buffer.p3, 'EPSG:3857', layer.crs).join(' '), transform(buffer.p4, 'EPSG:3857', layer.crs).join(' '), transform(buffer.p5, 'EPSG:3857', layer.crs).join(' ')];
+                        const queryParam = coords.join(' ');
+                        let raw = `<wfs:GetFeature service="WFS" outputFormat="application/json" version="1.1.0" xmlns:topp="http://www.openplans.org/topp" xmlns:wfs="http://www.opengis.net/wfs" xmlns="http://www.opengis.net/ogc" xmlns:gml="http://www.opengis.net/gml" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.1.0/wfs.xsd"><wfs:Query typeName="${layer.name}"><Filter><Intersects><PropertyName>${layer.geometry}</PropertyName><gml:Polygon srsName="${layer.crs}"><gml:exterior><gml:LinearRing><gml:posList>${queryParam}</gml:posList></gml:LinearRing></gml:exterior></gml:Polygon></Intersects></Filter></wfs:Query></wfs:GetFeature>`;
+                        let requestOptions = {
+                            method: 'POST',
+                            body: raw,
+                            mode: 'cors',
+                        };
+                        fetch(layer.url + 'wfs', requestOptions)
+                            .then(res => {
+                                if (!res.ok) {
+                                    dispatch(setToastColor('danger'));
+                                    dispatch(setMessage({
+                                        title: 'Fetch error',
+                                        message: 'Could not fetch data!'
+                                    }));
+                                };
+                                return res.json();
+                            })
+                            .then(obj => {
+                                if (obj.features[0]) {
+                                    dispatch(setResult(obj.features[0]));
+                                    dispatch(triggerIdentifyVisibility(true));
+                                }
+                                else {
+                                    dispatch(setToastColor('warning'));
+                                    dispatch(setMessage({
+                                        title: 'Warning',
+                                        message: 'No results found!'
+                                    }));
+                                    dispatch(triggerShowToast(true));
+                                }
+                            })
+                            .catch(error => {
+                                if (error.name !== 'AbortError') {
+                                    dispatch(setToastColor('danger'));
+                                    dispatch(setMessage({
+                                        title: 'Fetch error',
+                                        message: error.toString()
+                                    }));
+                                    dispatch(triggerShowToast(true));
+                                };
+                            });
+                    })
+                }
+                else {
+                    dispatch(setToastColor('warning'));
+                    dispatch(setMessage({
+                        title: 'Warning',
+                        message: 'No queriable layers found!'
+                    }));
+                    dispatch(triggerShowToast(true));
+                }
                 return () => {
                     controller.abort()
                 };
