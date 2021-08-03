03e32a61886995a10a0826b89830d57ccb50869a
ca5p3r <ca5p3r1@icloud.com>
2021-08-03 17:44:21 +0200

    adjust buffer and support identify multiple result

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,12 +1,19 @@
 import ImageWMS from 'ol/source/ImageWMS';
 import Image from 'ol/layer/Image';
-export const makeBuffer = (point) => {
+export const makeBuffer = (geom, point) => {
+    let buffer;
+    if (geom === 'Polygon' || geom === 'MultiPolygon') {
+        buffer = 5
+    }
+    else {
+        buffer = 100
+    };
     let x = point[0];
     let y = point[1];
-    let p1 = [x - 5, y + 5];
-    let p2 = [x + 5, y + 5];
-    let p3 = [x + 5, y - 5];
-    let p4 = [x - 5, y - 5];
+    let p1 = [x - buffer, y + buffer];
+    let p2 = [x + buffer, y + buffer];
+    let p3 = [x + buffer, y - buffer];
+    let p4 = [x - buffer, y - buffer];
     let p5 = p1;
     return { p1, p2, p3, p4, p5 }
 };
