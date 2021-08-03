fe0181d7d1071f6d37371d0b0c0031e7861d3dac
ca5p3r <ca5p3r1@icloud.com>
2021-08-03 15:18:02 +0200

    remove props from toast

--- src/app.js
+++ src/app.js
@@ -1,27 +1,20 @@
 import MyMap from './components/map';
 import Navbar from './components/navbar';
 import {
-    useSelector,
-    useDispatch
+    useSelector
 } from 'react-redux';
 import LandingPage from './components/landing';
 import Loading from './components/widgets/loader';
 import MyToast from './components/widgets/toast';
-import { triggerToast } from "./actions";
 function App() {
-    const dispatch = useDispatch();
     const login = useSelector(state => state.login.isLogged);
     const isLoading = useSelector(state => state.mapInfo.isLoading);
-    const toastInfo = useSelector(state => state.toast);
-    const handleToastTrigger = () => {
-        dispatch(triggerToast());
-    };
     return (
         <div className="App">
             <Navbar />
             {isLoading && <Loading />}
             {login ? <MyMap /> : <LandingPage />}
-            {toastInfo.visibility && <MyToast triggerShowToast={handleToastTrigger} color={toastInfo.color} visibility={toastInfo.visibility} title={toastInfo.title} message={toastInfo.message} />}
+            <MyToast />
         </div>
     );
 };
