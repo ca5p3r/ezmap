30532d009c1061f26d6d76590847661a7e91ba16
AhmedElabd96 <ahmedelabd9600@gmail.com>
2021-08-19 19:42:12 +0200

    mounirUpdates

---
+++ src/components/assets/index.js
@@ -0,0 +1,36 @@
+import { ReactComponent as LOGO } from "./svg/logo.svg";
+import { ReactComponent as SAVE } from "./svg/save.svg";
+import { ReactComponent as ROUTING } from "./svg/routing.svg";
+import { ReactComponent as EXPORT } from "./svg/export.svg";
+import { ReactComponent as IMPORT } from "./svg/import.svg";
+import { ReactComponent as SKETCHING } from "./svg/sketching.svg";
+import { ReactComponent as EDITING } from "./svg/editing.svg";
+import { ReactComponent as SPATIAL_SEARCH } from "./svg/spatialSearch.svg";
+import { ReactComponent as TABULAR_SEARCH } from "./svg/tabularSearch.svg";
+import { ReactComponent as SIMPLE_SEARCH } from "./svg/simpleSearch.svg";
+import { ReactComponent as IDENTIFY } from "./svg/identify.svg";
+import { ReactComponent as BOOKMARKS } from "./svg/bookmarks.svg";
+import { ReactComponent as LAYER_MANAGER } from "./svg/layerManager.svg";
+import { ReactComponent as TOC } from "./svg/toc.svg";
+import { ReactComponent as LOGOUT } from "./svg/logout.svg";
+import { ReactComponent as REGISTER } from "./svg/register.svg";
+import { ReactComponent as LOGIN } from "./svg/login.svg";
+export const svg = {
+  logo: <LOGO />,
+  save: <SAVE />,
+  routing: <ROUTING />,
+  export: <EXPORT />,
+  import: <IMPORT />,
+  sketching: <SKETCHING />,
+  editing: <EDITING />,
+  spatialSearch: <SPATIAL_SEARCH />,
+  tabularSearch: <TABULAR_SEARCH />,
+  simpleSearch: <SIMPLE_SEARCH />,
+  identify: <IDENTIFY />,
+  bookmarks: <BOOKMARKS />,
+  layerManager: <LAYER_MANAGER />,
+  toc: <TOC />,
+  logout: <LOGOUT />,
+  register: <REGISTER />,
+  login: <LOGIN />,
+};
