2b3bf37264f8f50b2543218c406dce313e30529d
ca5p3r <ca5p3r1@icloud.com>
2021-08-29 19:48:48 +0200

    enhancements

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ import { fileURLToPath } from 'url';
 import cors from 'cors';
 import morgan from 'morgan';
 import express from 'express';
+import helmet from 'helmet';
 import json from 'body-parser';
 import { authRouters } from './routes/auth.js';
 import { settingsRouters } from './routes/settings.js';
@@ -15,6 +16,10 @@ const __dirname = dirname(__filename);
 const schema = readFileSync(resolve(__dirname, "./assets/schema.sql")).toString();
 
 const app = express();
+app.disable("x-powered-by");
+
+let safeApp = express();
+safeApp.use(helmet.hidePoweredBy());
 
 pool.connect((connError, client, done) => {
     if (connError) return done(connError);
@@ -22,23 +27,27 @@ pool.connect((connError, client, done) => {
         done();
         if (clientError) console.log(clientError);
         else {
-            app.listen(9090);
+            safeApp.listen(9090);
         }
     });
 });
 
-app.use(cors());
-app.use(json());
-app.use(morgan('dev'));
+let corsOptions = {
+    origin: ['http://localhost:3000']
+};
 
-app.get('/', (_, res) => {
+safeApp.use(cors(corsOptions));
+safeApp.use(json());
+safeApp.use(morgan('dev'));
+
+safeApp.get('/', (_, res) => {
     res.send('<div><h3>Homepage</h3><p>This is the homepage endpoint for the backend server!</p></div>');
 });
 
-app.use('/authService', authRouters);
-app.use('/configService', settingsRouters);
-app.use('/queryService', queryRouters);
+safeApp.use('/authService', authRouters);
+safeApp.use('/configService', settingsRouters);
+safeApp.use('/queryService', queryRouters);
 
-app.use((_, res) => {
+safeApp.use((_, res) => {
     res.status(404).send('<div><h3>Not Found</h3><p>You have reached an undefined endpoint!</p></div>');
 });
