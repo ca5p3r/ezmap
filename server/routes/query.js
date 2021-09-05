32c20234962324283d9af7cbc20b7ff134ae9248
ca5p3r <ca5p3r1@icloud.com>
2021-09-05 10:48:55 +0200

    remove highlight

--- server/routes/query.js
+++ server/routes/query.js
@@ -1,8 +1,7 @@
 import { Router } from 'express';
-import { identify, spatial_search, simple_search, coordsToPolygon } from '../controllers/query.js';
+import { identify, spatial_search, simple_search } from '../controllers/query.js';
 
 export const queryRouters = Router();
 queryRouters.post('/identify', identify);
 queryRouters.post('/ssearch', spatial_search);
 queryRouters.post('/tsearch', simple_search);
-queryRouters.post('/createPolygon', coordsToPolygon);
