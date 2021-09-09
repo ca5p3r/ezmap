db3b98c605981e7818c8df39018769248ca9eee3
ca5p3r <ca5p3r1@icloud.com>
2021-09-09 21:19:41 +0200

    add overlay to loader

--- src/components/containers/Loading.js
+++ src/components/containers/Loading.js
@@ -1,7 +1,12 @@
+import { useSelector } from "react-redux";
+
 const Loading = () => {
+    const isLoading = useSelector(state => state.mapInfo.isLoading);
     return (
-        <div className="spinner-grow text-info" style={{ width: "6rem", height: "6rem", position: "absolute", bottom: "25rem", left: "54rem", zIndex: "2000" }} role="status" >
-            <span className="visually-hidden"></span>
+        <div className="overlay" style={{ display: isLoading ? 'block' : 'none' }}>
+            <div className="spinner-grow text-info" style={{ width: "6rem", height: "6rem", position: "absolute", bottom: "25rem", left: "54rem", zIndex: "2000" }} role="status" >
+                <span className="visually-hidden"></span>
+            </div>
         </div>
     );
 };
